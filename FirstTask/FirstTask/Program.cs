f7478348e8bc7d001a6ffb4089bd40b361f45a79
YaroslavKormushyn <kormushyn.slavik@gmail.com>
2017-08-28 11:09:40 +0300

    Basic code review and cleanup.

--- FirstTask/FirstTask/Program.cs
+++ FirstTask/FirstTask/Program.cs
@@ -28,9 +28,9 @@ namespace FirstTask
 		{
 			Console.WriteLine(TASKINFO_MSG, "86a");
 			Console.WriteLine("Enter a positive number: ");
-			int n = int.Parse(Console.ReadLine());
+			int number = int.Parse(Console.ReadLine());
 
-			Console.WriteLine($"Digits: {n.ToString().Length}");
+			Console.WriteLine($"Digits: {number.ToString().Length}");
 			Console.ReadLine();
 		}
 
@@ -39,8 +39,8 @@ namespace FirstTask
 			Console.WriteLine(TASKINFO_MSG, "178b");
 			Console.WriteLine(
 				"Enter a sequence of positive numbers (divided by space): ");
-			var seq = new List<int>();
-			var rawSeq = Console.ReadLine().Split(' ');
+			var seq = new List<int>();                  //Result sequence of integers.
+			var rawSeq = Console.ReadLine().Split(' '); //Sequence of integerss as strings.
 			foreach (var number in rawSeq)
 			{
 				seq.Add(int.Parse(number));
@@ -61,17 +61,22 @@ namespace FirstTask
 		{
 			Console.WriteLine(TASKINFO_MSG, "332");
 			Console.WriteLine("Enter a positive number: ");
+			//The number to represent as a^2+b^2+c^2+d^2.
 			int n = int.Parse(Console.ReadLine());
 
+			//First result number.
 			int a = (int)Math.Sqrt(n) - 1;
 			while (a > 0)
 			{
+				//Second result number.
 				int b = (int)Math.Sqrt(n - a * a) - 1;
 				while (b > 0)
 				{
+					//Third result number.
 					int c = (int)Math.Sqrt(n - a * a - b * b);
 					while (c > 0)
 					{
+						//Fourth result number.
 						int d = (int)Math.Sqrt(n - a * a - b * b - c * c);
 						if (d < 1)
 							c--;
