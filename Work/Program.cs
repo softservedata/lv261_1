bb72c2aa19071b738de37936991014604076d0b6
garasym <garasym@gmail.com>
2017-08-29 19:49:32 +0300

    Modify

--- Work/Program.cs
+++ Work/Program.cs
@@ -6,10 +6,11 @@ using System.Threading.Tasks;
 
 namespace Work
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            Console.WriteLine("Ok2");
         }
     }
 }
