2a5190318ef9dede183b290f1aa6a655d735f9c6
Serhii Zinkiv <agrail1307@gmail.com>
2017-09-05 11:28:18 +0300

    add new version js

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,23 +1,24 @@
 function print(data){
     document.write(`${data}<br>`);
 }
-let number = prompt();
-let rez = [];
-function getMeXY(number){
-    let _x; let _y;
+let number = prompt('Enter your number!');
+// task 243
+function powerSumTwoNumberXY(number){
+    let _x; let _y; let rezult = [];
     for ( _x = 1; _x < Math.sqrt(number); _x++) {
         _y = Math.sqrt(number - Math.pow(_x,2));
         let temp = Math.floor(_y);
         if(temp === _y)
             {
-                rez.push(_x + " " + _y);
+                rezult.push(_x + " " + _y);
             }
             else continue;  
     }
-    return rez;
+    return rezult;
 }
-function getMeXYZ(number) {
-    let _x; let _y; let _z;
+// task 331
+function powerSumThreeNumberXYZ(number) {
+    let _x; let _y; let _z; let rezult = [];
     for (_x = 1; _x < Math.sqrt(number); _x++) {
         for (_y = 1; _y < Math.sqrt(number - Math.pow(_x,2)); _y++) {
             _z = Math.sqrt(number - Math.pow(_x,2) - Math.pow(_y,2));
@@ -28,14 +29,15 @@ function getMeXYZ(number) {
                 }
             if (temp === _z)
                 {
-                    rez.push(_x + " " + _y + " " + _z);
+                    rezult.push(_x + " " + _y + " " + _z);
                 }
             else continue;
         }
    }
-   return rez;
+   return rezult;
 }
-function getPowerLog(m) {
+// Task 107
+function maxLog(m) {
     let p; const POWER = 4;
     p = Math.log(m)/Math.log(POWER);
     let temp = Math.floor(p);
@@ -49,4 +51,7 @@ function getPowerLog(m) {
         }
     return p;
 }
-print(getPowerLog(number));
+
+print("Max number: " + maxLog(number));
+print("X^2 + Y^2 = " + number + " : " + powerSumTwoNumberXY(number));
+print("X^2 + Y^2 + Z^2 = " + number + " : " + powerSumThreeNumberXYZ(number));
